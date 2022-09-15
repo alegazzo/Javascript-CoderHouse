005e8ab4eec8f772e929031b91e1a2621ac517ec
alegazzo <72534448+alegazzo@users.noreply.github.com>
2022-09-14 22:02:25 -0300

    agregando eventos

--- Proyecto Final/js/app.js
+++ Proyecto Final/js/app.js
@@ -8,7 +8,13 @@ let arrayProductos = [];
 
 let divProductos = document.getElementById("divProductos");
 
-let divSaludoInicial = document.getElementById("divSaludo");
+let tituloSaludo = document.getElementById("saludo");
+
+let formProductos = document.getElementById("formProductos");
+
+let btnFiltrar = document.getElementById("btnFiltrar");
+
+let inputFiltro = document.getElementById("inputFiltro");
 
 // Objetos
 
@@ -34,37 +40,23 @@ class ItemCarrito {
 //Funciones
 const agregarProducto = () => {
 
-    let id = prompt("Ingrese id del producto");
+    let id = document.getElementById("idProducto").value;
+    let nombre = document.getElementById("nombreProducto").value;
+    let precio = document.getElementById("precioProducto").value;
+    let tipo = document.getElementById("tipoProducto").value;
 
-    let nombre = prompt("Ingrese el nombre del producto");
-
-    let precio = parseInt(prompt("Ingrese el precio del producto"));
-
-    let tipo = prompt("Detalle el Tipo de Producto");
-
+    
     let producto = new Producto(id,nombre,precio,tipo);
+    console.log(producto)
 
     let validacion = arrayProductos.findIndex(x => x.id == id);
 
-    while(validacion != -1){
-
-        alert("ya existe ese nombre de producto");
-
-        id = prompt("Ingrese id del producto");
-
-        nombre = prompt("Ingrese el nombre del producto");
-
-        precio = parseInt(prompt("Ingrese el precio del producto"));
-
-        tipo = prompt("Detalle el Tipo de Producto");
-
-        producto = new Producto(nombre, precio);
-
-        validacion = arrayProductos.findIndex(x => x.id == id);
-
+    if(validacion != -1){
+        alert("ya existe ese id de producto");
     }
-
-    arrayProductos.push(producto);
+    else{
+        arrayProductos.push(producto);
+    }
 }
 
 const eliminarProducto = (id) => {
@@ -95,7 +87,8 @@ const filtrarProductosXNombre = (nombre) => {
 
     let arrayFiltrado = arrayProductos.filter(x => x.nombre == nombre);
 
-    return arrayFiltrado;
+    cargarProductosFiltrados(arrayFiltrado);
+
 }
 
 // crea una card de producto
@@ -107,10 +100,12 @@ const crearCardProducto = (producto) => {
 
     nuevaCard.innerHTML = `<img src="img/prueba.jpg" class="card-img-top" alt="...">
                             <div class="card-body">
+                                <h4 class="card-title">${producto.id}</h4>
                                 <h5 class="card-title">${producto.nombre}</h5>
                                 <p class="card-text">$${producto.precio}</p>
-                                <p class="card-text">Tipo: ${producto.tipo}</p>
-                                <a href="#" class="btn btn-primary">Add to Cart</a>
+                                <p class="card-text">Type: ${producto.tipo}</p>
+                                <a id="addProducto" href="" class="btn btn-primary">Add to Cart</a>
+                                <a id="deleteProducto" href="" class="btn btn-primary">Delete Product</a>
                             </div>`;
 
     divProductos.append(nuevaCard);
@@ -126,49 +121,81 @@ const  cargarProductos = () => {
     }
 }
 
-// agrega el nombre del usuario al saludo
-const saludo = (nombre) => {
-    divSaludoInicial.innerHTML = `Bienvenido a mi Ecommerce ${nombre}!`;
+const  cargarProductosFiltrados = (arrayFiltrado) => {
+
+    divProductos.innerHTML = ``;
+    for (const item of arrayFiltrado) {
+        crearCardProducto(item);
+    }
 }
 
-//APLICACION. 
+// agrega el nombre del usuario al saludo
+const saludo = (nombre) => {
+    tituloSaludo.innerHTML = `Bienvenido a mi Ecommerce ${nombre}!`;
+}
 
-//EVENTO ON LOAD PARA CARGAR ALGO DEL HTML PRIMERO...
-window.addEventListener('load', (event) => {
+//****************************************
+//EVENTOS
 
-    let nombreUsuario = prompt("Bienvenido, ingrese su nombre por favor...");
-    let cantProductos = parseInt(prompt("Ingrese la cantidad de productos a agregar"));
+formProductos.addEventListener('submit', (e) =>{
 
-    while(isNaN(cantProductos)){
+    e.preventDefault();
 
-        cantProductos = parseInt(prompt("Ingrese la cantidad de productos a agregar"));
-    }
-
-
-    for (let i = 0; i < cantProductos; i++) {
-        
-        agregarProducto();
-        
-    }
+    agregarProducto();
 
     cargarProductos();
-    saludo(nombreUsuario);
 
-    //TIMEOUT PARA CARGAR EL HTML Y LUEGO MOSTRAR EL PROMPT.
-    setTimeout(() => {
-        let eliminar = prompt("desea eliminar algun producto? S/N").toUpperCase();
-
-        if(eliminar == 'S'){
-
-            eliminar = prompt("ingrese el id del producto a eliminar");
-
-            eliminarProducto(eliminar);
-
-            cargarProductos();
-        }
-    }, 2000);
-    
 });
+
+btnFiltrar.addEventListener('click', (e) => {
+
+    let nombre = inputFiltro.value;
+    filtrarProductosXNombre(nombre);
+
+});
+
+
+//APLICACION. 
+let nombreUsuario = prompt("Bienvenido, ingrese su nombre por favor...");
+saludo(nombreUsuario);
+
+
+
+//EVENTO ON LOAD PARA CARGAR ALGO DEL HTML PRIMERO...
+// window.addEventListener('load', (event) => {
+
+    
+//     let cantProductos = parseInt(prompt("Ingrese la cantidad de productos a agregar"));
+
+//     while(isNaN(cantProductos)){
+
+//         cantProductos = parseInt(prompt("Ingrese la cantidad de productos a agregar"));
+//     }
+
+
+//     for (let i = 0; i < cantProductos; i++) {
+        
+//         agregarProducto();
+        
+//     }
+
+//     cargarProductos();
+    
+//     //TIMEOUT PARA CARGAR EL HTML Y LUEGO MOSTRAR EL PROMPT.
+//     setTimeout(() => {
+//         let eliminar = prompt("desea eliminar algun producto? S/N").toUpperCase();
+
+//         if(eliminar == 'S'){
+
+//             eliminar = prompt("ingrese el id del producto a eliminar");
+
+//             eliminarProducto(eliminar);
+
+//             cargarProductos();
+//         }
+//     }, 2000);
+    
+// });
 
 
 
